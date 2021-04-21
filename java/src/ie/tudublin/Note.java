2d9e7be0ee81ef9b70016d309a296de7ddea320c
ChristinaAlferieva <C19401882@mytudublin.ie>
2021-04-21 19:41:47 +0100

    draw

---
+++ java/src/ie/tudublin/Note.java
@@ -0,0 +1,23 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+
+import processing.core.PApplet;
+
+public class Note {
+    private char note;
+    private int duration;
+
+    public Note()
+    {
+
+    }
+    
+    @Override
+    public String toString(){
+        return "";
+    }
+}
+
+
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -9,10 +9,13 @@ import processing.data.TableRow;
 
 public class ScoreDisplay extends PApplet
 {
-	public ArrayList<Note> convertStringToArrayList(String score){
+	public ArrayList<Note> convertStringToArrayList(String score)
+	{
 		ArrayList<Note> notes = new ArrayList<Note>();
 	}
 	
+	private float leftBorder;
+	private float border;
 
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
@@ -36,7 +39,7 @@ public class ScoreDisplay extends PApplet
 
 			for(int i = score.length() - 1; i >= 0; i--)
 			{
-				if(score[] = 1)
+				if(duration = 1)
 				{
 					System.out.println("Quaver");
 				}
@@ -65,10 +68,37 @@ public class ScoreDisplay extends PApplet
 	public void draw()
 	{
 		background(255);
-		
+		drawNotes();
+		textAlign(CENTER, CENTER);
+
+		for(int i = 0; i <= 8; i++)
+		{
+			float x = map(i, 0, 8, leftBorder, width - border);
+			drawLine(10, 50, 200, 50);
+			text(i, x, border/2);	
+		}
+
+		for(int i = 0; i <= score.size; i++)
+		{
+			float y = map(i, 0, score.size(), border + 50, height - border - 20);
+            Note t = score.get(i);
+            float x1 = map(t.getStart(), 1, 30, leftBorder, width - border);
+            float x2 = map(t.getEnd(), 1, 50, leftBorder, width - border);
+            int c = (int) map(i, 0, score.size(), 0, 50);
+            fill(c, 255, 255);
+            rect(x1, y - 20, x2 - x1, 40);
+            fill(255);
+            textAlign(CENTER, CENTER);
+            text(t.getTask(), 20, y);
+		}
+			
 	}
 
 	void drawNotes()
 	{
+		for(int i = score.length() - 1; i >= 0; i--)
+		{
+			score.render(this)
+		}
 	}
 }
